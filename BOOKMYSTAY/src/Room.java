f318aa1f93c9c328294a9b7769a674a5843e5c06
vendhankalai614-droid <vendhankalai614@gmail.com>
2026-03-24 10:35:31 +0530

    Room.java

--- BOOKMYSTAY/src/Room.java
+++ BOOKMYSTAY/src/Room.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+abstract class Room {
 
-public class Room {
+    // Common attributes
+    private String roomType;
+    private int numberOfBeds;
+    private double pricePerNight;
+
+    // Constructor
+    public Room(String roomType, int numberOfBeds, double pricePerNight) {
+        this.roomType = roomType;
+        this.numberOfBeds = numberOfBeds;
+        this.pricePerNight = pricePerNight;
+    }
+
+    // Method to display room details
+    public void displayRoomDetails() {
+        System.out.println("Room Type       : " + roomType);
+        System.out.println("Number of Beds  : " + numberOfBeds);
+        System.out.println("Price per Night : $" + pricePerNight);
+    }
 }
