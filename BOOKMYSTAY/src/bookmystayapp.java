92f1279bf8a19eda1fa4e1c6e1a75d4767dc4f51
vendhankalai614-droid <vendhankalai614@gmail.com>
2026-03-24 10:36:47 +0530

    bookmystayapp.java

--- BOOKMYSTAY/src/bookmystayapp.java
+++ BOOKMYSTAY/src/bookmystayapp.java
@@ -1,14 +1,37 @@
 public class bookmystayapp {
+
     public static void main(String[] args) {
 
-        // Printing welcome message
         System.out.println("=====================================");
-        System.out.println(" Welcome to Hotel Booking System ");
-        System.out.println(" Version: 1.0 ");
+        System.out.println(" Book My Stay App ");
+        System.out.println(" Version: 2.1 ");
         System.out.println("=====================================");
 
-        // Inform user about successful startup
-        System.out.println("Application started successfully.");
+        // Creating room objects (Polymorphism)
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
 
+        // Static availability variables
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
+
+        // Display details
+        System.out.println("\n--- Room Details & Availability ---\n");
+
+        singleRoom.displayRoomDetails();
+        System.out.println("Availability    : " + singleRoomAvailability);
+        System.out.println("-------------------------------------");
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Availability    : " + doubleRoomAvailability);
+        System.out.println("-------------------------------------");
+
+        suiteRoom.displayRoomDetails();
+        System.out.println("Availability    : " + suiteRoomAvailability);
+        System.out.println("-------------------------------------");
+
+        System.out.println("\nApplication execution completed.");
     }
 }
