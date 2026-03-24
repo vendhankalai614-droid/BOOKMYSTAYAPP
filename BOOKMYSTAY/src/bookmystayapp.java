c164b160ca7007ed4eb1947ab1798a58f675e320
vendhankalai614-droid <vendhankalai614@gmail.com>
2026-03-24 09:16:20 +0530

    uc1 welcome

---
+++ BOOKMYSTAY/src/bookmystayapp.java
@@ -0,0 +1,14 @@
+public class bookmystayapp {
+    public static void main(String[] args) {
+
+        // Printing welcome message
+        System.out.println("=====================================");
+        System.out.println(" Welcome to Hotel Booking System ");
+        System.out.println(" Version: 1.0 ");
+        System.out.println("=====================================");
+
+        // Inform user about successful startup
+        System.out.println("Application started successfully.");
+
+    }
+}
